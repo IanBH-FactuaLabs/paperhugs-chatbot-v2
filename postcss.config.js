fa14a22e4ebbe3d215361c3c18e775fb538717bf
IanBH-FactuaLabs <ian@factualabs.com>
2025-06-06 12:14:14 -0400

    tailwind reconfig

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+    plugins: {
+      '@tailwindcss/postcss': {}, // ✅ This replaces 'tailwindcss'
+      autoprefixer: {}
+    }
+  };
